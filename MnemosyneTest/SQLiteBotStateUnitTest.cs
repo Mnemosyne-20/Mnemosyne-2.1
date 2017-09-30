f724364424efafe10f49dac73801b4ccffa93d9d
chuggafan <chuggafans@gmail.com>
2017-09-30 16:10:45 -0400

    Working unit tests

--- MnemosyneTest/SQLiteBotStateUnitTest.cs
+++ MnemosyneTest/SQLiteBotStateUnitTest.cs
@@ -6,23 +6,18 @@ namespace MnemosyneTest
     [TestClass]
     public class SQLiteBotStateUnitTest
     {
-        [TestInitialize]
-        public void Init()
-        {
-            System.IO.File.Create("Test.sqlite");
-        }
-        [TestCategory("SQLiteBotState")]
-        [DeploymentItem("Test.sqlite", "1")]
         [TestMethod]
+        [TestCategory("SQLiteBotState")]
+        [DeploymentItem("Test.sqlite", "Data\\1")]
         public void TestAddBotCommentSqlite()
         {
             SQLiteBotState sqliteBotState = new SQLiteBotState("1\\Test.sqlite");
             sqliteBotState.AddBotComment("sad", "postcomment");
             Assert.IsTrue(sqliteBotState.GetCommentForPost("sad") == "postcomment");
         }
-        [DeploymentItem("Test.sqlite", "2")]
-        [TestCategory("SQLiteBotState")]
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "Data\\2")]
+        [TestCategory("SQLiteBotState")]
         public void TestCheckCommentSqlite()
         {
             SQLiteBotState sqliteBotState = new SQLiteBotState("2\\Test.sqlite");

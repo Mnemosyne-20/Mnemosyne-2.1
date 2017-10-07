a39bfca005106d86f45fee8a1c602041e0d76050
chuggafan <chuggafans@gmail.com>
2017-10-07 13:17:50 -0400

    Minor spelling fix

--- Mnemosyne2Reborn/Program.cs
+++ Mnemosyne2Reborn/Program.cs
@@ -49,7 +49,7 @@ namespace Mnemosyne2Reborn
         /// <summary>
         /// This is intentional to be this way, it's so that the editor can get the headers easily
         /// </summary>
-        public static readonly string[] Headers = new string[] { "Archives for links in this post:\n\n", "Archive for this post:\n\n", "Archives for the links in comments:\n\n", "----\nI am Mnemosyne 2.1, {0} ^^^^/r/botsrights ^^^^[Contribute](https://github.com/Mnemosyne-20/Mnemosyne-2.1) ^^^^message ^^^^me ^^^^suggestions ^^^^at ^^^^any ^^^^time ^^^^Opt ^^^^out ^^^^of ^^^^tracking ^^^^by ^^^^messaging ^^^^me ^^^^\"Opt ^^^^Out\" ^^^^at ^^^^any ^^^^time" };
+        public static readonly string[] Headers = new string[] { "Archives for this post:\n\n", "Archive for this post:\n\n", "Archives for the links in comments:\n\n", "----\nI am Mnemosyne 2.1, {0} ^^^^/r/botsrights ^^^^[Contribute](https://github.com/Mnemosyne-20/Mnemosyne-2.1) ^^^^message ^^^^me ^^^^suggestions ^^^^at ^^^^any ^^^^time ^^^^Opt ^^^^out ^^^^of ^^^^tracking ^^^^by ^^^^messaging ^^^^me ^^^^\"Opt ^^^^Out\" ^^^^at ^^^^any ^^^^time" };
         /// <summary>
         /// These three being separate is important because it is used for data tracking
         /// </summary>

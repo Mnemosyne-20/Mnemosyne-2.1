a18d3a6d12797180d307380013f0ffd3fa79fb8e
chuggafan <chuggafans@gmail.com>
2017-10-07 17:23:49 -0400

    Memento tests yay

--- ArchiveApi/TimeMap/Mementos.cs
+++ ArchiveApi/TimeMap/Mementos.cs
@@ -5,23 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 namespace ArchiveApi
 {
-    public class Memento
+    public class Memento : IEquatable<Uri>, IEquatable<string>
     {
         public bool IsFirst => webLink.Attributes.GetValues("rel").Contains("first");
         public bool IsLast => webLink.Attributes.GetValues("rel").Contains("last");
-        public DateTime TimeArchived { get => DateTime.Parse(string.Join(" ", webLink.Attributes.GetValues("datetime"))); }
+        public DateTime TimeArchived => DateTime.Parse(string.Join(" ", webLink.Attributes.GetValues("datetime")));
         public bool IsActuallyMemento => webLink.Attributes.GetValues("rel").Contains("memento");
+        public Uri Url => new Uri(webLink.Uri);
         WebLink webLink;
         public Memento(WebLink memento)
         {
             webLink = memento;
         }
+        public override bool Equals(object other)
+        {
+            return (other is WebLink) && ((other as WebLink) == webLink);
+        }
+        public static bool operator ==(Memento mem1, Memento mem2) { return mem1.Equals(mem2); }
+        public static bool operator !=(Memento mem1, Memento mem2) { return !mem1.Equals(mem2); }
+        public static bool operator ==(Memento mem1, string mem2) { return mem1.Equals(mem2); }
+        public static bool operator !=(Memento mem1, string mem2) { return !mem1.Equals(mem2); }
+        public bool Equals(Uri other) { return Url == other; }
+        public bool Equals(string other) { return Url.ToString() == other; }
+        public override int GetHashCode() => base.GetHashCode();
     }
     public class Mementos : IEnumerable
     {
         public string TimeGate { get; private set; }
         public TimeMap TimeMap { get; private set; }
         public string Original { get; private set; }
+        public Memento FirstMemento => _mementos[0];
+        public Memento LastMemento => _mementos[_mementos.Length - 1];
         Memento[] _mementos;
         public Mementos(IEnumerable<Memento> mementoList)
         {
@@ -30,16 +44,24 @@ namespace ArchiveApi
             TimeMap = null;
             Original = null;
         }
-        internal Mementos(IEnumerable<WebLink> mementoList)
+        public Mementos(IEnumerable<WebLink> mementoList)
         {
-            foreach (var link in mementoList)
+            int mementoCount = (mementoList.Where(a => a.Attributes.GetValues("rel").Contains("memento"))).Count();
+            _mementos = new Memento[mementoCount];
+            for(int i = 0, i2 = 0; i < mementoList.Count(); i++)
             {
+                var link = mementoList.ElementAt(i);
                 if (link.Attributes.GetValues("rel").Contains("timegate"))
                     TimeGate = link.Attributes.GetValues("rel").First();
                 else if (link.Attributes.GetValues("rel").Contains("timemap"))
                     TimeMap = new TimeMap(link);
                 else if (link.Attributes.GetValues("rel").Contains("original"))
                     Original = link.Uri;
+                else if (link.Attributes.GetValues("rel").Contains("memento"))
+                {
+                    _mementos[i2] = new Memento(link);
+                    i2++;
+                }
             }
         }
         public IEnumerator GetEnumerator()
---
+++ ArchiveApiTest/MementoUnitTEST.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArchiveApi;
+using CoAP;
+namespace ArchiveApiTest
+{
+    [TestClass]
+    public class MementoUnitTEST
+    {
+        static string Test = @"<http://kernel.org/>; rel=""original"",
+<http://archive.is/timegate/http://kernel.org/>; rel=""timegate"",
+<http://archive.is/20120523210412/http://kernel.org/>; rel=""first memento""; datetime=""Wed, 23 May 2012 21:04:12 GMT"",
+<http://archive.is/20130109172600/http://kernel.org/>; rel=""memento""; datetime=""Wed, 09 Jan 2013 17:26:00 GMT"",
+<http://archive.is/20130224014512/http://kernel.org/>; rel=""memento""; datetime=""Sun, 24 Feb 2013 01:45:12 GMT"",
+<http://archive.is/20131125152406/https://kernel.org/>; rel=""memento""; datetime=""Mon, 25 Nov 2013 15:24:06 GMT"",
+<http://archive.is/20140601131731/http://kernel.org/>; rel=""memento""; datetime=""Sun, 01 Jun 2014 13:17:31 GMT"",
+<http://archive.is/20150401170814/http://kernel.org/>; rel=""memento""; datetime=""Wed, 01 Apr 2015 17:08:14 GMT"",
+<http://archive.is/20150703202917/https://kernel.org/>; rel=""memento""; datetime=""Fri, 03 Jul 2015 20:29:17 GMT"",
+<http://archive.is/20150715032634/http://kernel.org/>; rel=""memento""; datetime=""Wed, 15 Jul 2015 03:26:34 GMT"",
+<http://archive.is/20150908032902/https://kernel.org/>; rel=""memento""; datetime=""Tue, 08 Sep 2015 03:29:02 GMT"",
+<http://archive.is/20150922214548/https://kernel.org/>; rel=""memento""; datetime=""Tue, 22 Sep 2015 21:45:48 GMT"",
+<http://archive.is/20151013090704/https://kernel.org/>; rel=""memento""; datetime=""Tue, 13 Oct 2015 09:07:04 GMT"",
+<http://archive.is/20151030023534/https://kernel.org/>; rel=""memento""; datetime=""Fri, 30 Oct 2015 02:35:34 GMT"",
+<http://archive.is/20160102094030/https://kernel.org/>; rel=""memento""; datetime=""Sat, 02 Jan 2016 09:40:30 GMT"",
+<http://archive.is/20160204181713/https://kernel.org/>; rel=""memento""; datetime=""Thu, 04 Feb 2016 18:17:13 GMT"",
+<http://archive.is/20160416010219/https://kernel.org/>; rel=""memento""; datetime=""Sat, 16 Apr 2016 01:02:19 GMT"",
+<http://archive.is/20161021231009/https://kernel.org/>; rel=""memento""; datetime=""Fri, 21 Oct 2016 23:10:09 GMT"",
+<http://archive.is/20170308191942/https://kernel.org/>; rel=""last memento""; datetime=""Wed, 08 Mar 2017 19:19:42 GMT"",
+<http://archive.is/timemap/http://kernel.org/>; rel=""self""; type=""application/link-format""; from=""Wed, 23 May 2012 21:04:12 GMT""; until=""Wed, 08 Mar 2017 19:19:42 GMT""";
+        [TestMethod]
+        public void MementoFirstTest()
+        {
+            Mementos mementos = new Mementos(LinkFormat.Parse(Test));
+            Assert.IsTrue(mementos.FirstMemento == "http://archive.is/20120523210412/http://kernel.org/");
+        }
+        [TestMethod]
+        public void MementoLastTest()
+        {
+            Mementos mementos = new Mementos(LinkFormat.Parse(Test));
+            Assert.IsTrue(mementos.LastMemento == "http://archive.is/20170308191942/https://kernel.org/");
+        }
+    }
+}

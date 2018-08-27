c753f813bc7e436b4113ddb513011673d3235f4e
chuggafan <chuggafans@gmail.com>
2018-08-26 22:26:53 -0400

    Remove instrumentation

--- Mnemosyne2Reborn/Commenting/PostArchives.cs
+++ Mnemosyne2Reborn/Commenting/PostArchives.cs
@@ -191,7 +191,6 @@ namespace Mnemosyne2Reborn.Commenting
             Console.Title = $"Posting new comment to comment {comment.Id}";
             string c = head + string.Join("", ArchiveList) + "\n" + string.Format(Program.Headers[3], config.FlavorText[rand.Next(0, config.FlavorText.Length)]);
             Comment botComment = comment.Reply(c);
-            InternalLogger.Log($"Attempting to update bot comment for post: {comment.LinkId}: {comment.Id}");
             try
             {
                 state.UpdateBotComment(comment.LinkId.Substring(3), botComment.Id);
@@ -203,7 +202,6 @@ namespace Mnemosyne2Reborn.Commenting
                 InternalLogger.EnhancedLog($"Exception generated and caught replying to comment {comment.Id} with new comment {Regex.Replace(botComment.Id, "t1_", "")}: ", e);
                 botComment.Del();
             }
-            InternalLogger.Log($"Comment with ID {comment.Id} has been updated to comment {botComment.Id} for post {comment.LinkId}");
         }
         /// <summary>
         /// Posts all links archived
